fdc52103ccfa0ba5545ef8ade49af177dd414606
yuta-49 <yumon.f2549@gmail.com>
2021-12-05 18:58:29 +0900

    認証処理の実装を追加
    Spring Securityに関する認証処理を追加

---
+++ book-manager/src/main/kotlin/com/book/manager/authentication/service/BookManagerUserDetailsService.kt
@@ -0,0 +1,48 @@
+package com.book.manager.authentication.service
+
+import com.book.manager.authentication.domain.User
+import com.book.manager.domain.enum.RoleType
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+
+//認証処理の実装
+class BookManagerUserDetailsService(private val authenticationService: AuthenticationService) : UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails? {
+        val user = authenticationService.findUser(username)
+        return user?.let { BookManagerUserDetails(user) }
+    }
+}
+
+data class BookManagerUserDetails(val id: Long, val email: String, val pass: String, val roleType: RoleType) : UserDetails {
+    constructor(user: User) : this(user.id, user.email, user.password, user.roleType)
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return AuthorityUtils.createAuthorityList(this.roleType.toString())
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+    override fun getUsername(): String {
+        return this.email
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun getPassword(): String {
+        return this.pass
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+}
--- book-manager/src/main/kotlin/com/book/manager/presentation/config/SecurityConfig.kt
+++ book-manager/src/main/kotlin/com/book/manager/presentation/config/SecurityConfig.kt
@@ -1,18 +1,44 @@
 package com.book.manager.presentation.config
 
+import com.book.manager.authentication.service.AuthenticationService
+import com.book.manager.authentication.service.BookManagerUserDetailsService
+import com.book.manager.domain.enum.RoleType
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @EnableWebSecurity
-class SecurityConfig() : WebSecurityConfigurerAdapter() {
+class SecurityConfig(private val authenticationService: AuthenticationService) : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
-        http.csrf().disable()
+        http.authorizeRequests()
+            .mvcMatchers("/login").permitAll()
+            .mvcMatchers("/admin/**").hasAuthority(RoleType.ADMIN.toString())
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+            .formLogin()
+            .loginProcessingUrl("/login")
+            .usernameParameter("email")
+            .passwordParameter("pass")
+//            .successHandler(BookManagerAuthenticationSuccessHandler())
+//            .failureHandler(BookManagerAuthenticationFailureHandler())
+            .and()
+//            .exceptionHandling()
+//            .authenticationEntryPoint(BookManagerAuthenticationEntryPoint())
+//            .accessDeniedHandler(BookManagerAccessDeniedHandler())
+//            .and()
             .cors()
             .configurationSource(corsConfigurationSource())
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(BookManagerUserDetailsService(authenticationService))
+            .passwordEncoder(BCryptPasswordEncoder())
     }
 
     private fun corsConfigurationSource(): CorsConfigurationSource {

e974fc499bb8b48706fdbe621b94d4931fb3409c
yuta-49 <yumon.f2549@gmail.com>
2021-12-07 11:36:02 +0900

    返却機能を実装

--- book-manager/src/main/kotlin/com/book/manager/infrastructure/database/service/RentalService.kt
+++ book-manager/src/main/kotlin/com/book/manager/infrastructure/database/service/RentalService.kt
@@ -1,10 +1,11 @@
-package com.book.manager.infrastructure.database.repository
+package com.book.manager.infrastructure.database.service
 
 import com.book.manager.authentication.repository.UserRepository
 import com.book.manager.domain.model.Rental
 import com.book.manager.domain.repository.BookRepository
 import com.book.manager.domain.repository.RentalRepository
 import com.book.manager.infrastructure.database.record.RentalDynamicSqlSupport
+import com.book.manager.infrastructure.database.record.RentalDynamicSqlSupport.Rental.userId
 import com.book.manager.infrastructure.database.record.RentalRecord
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,6 +18,9 @@ private const val RENTAL_TERM_DAYS = 14L
 class RentalService(private val userRepository: UserRepository, private val bookRepository: BookRepository,
     private val rentalRepository: RentalRepository) {
 
+    /**
+     * 貸出機能のロジック
+     */
     @Transactional
     fun startRental(bookId: Long, userId: Long){
         //ユーザーのチェック
@@ -35,5 +39,21 @@ class RentalService(private val userRepository: UserRepository, private val book
         rentalRepository.startRental(rental)
     }
 
+    /**
+     * 返却機能のロジック
+     */
+    @Transactional
+    fun endRental(bookId: Long, userId: Long){
+        //ユーザーが存在するかチェック
+        userRepository.findByPrimaryKey(userId) ?: throw IllegalArgumentException("該当するユーザーが存在しません userId:${userId}")
 
+        //本が存在するかチェック
+        val book = bookRepository.findWithRental(bookId)?: throw IllegalArgumentException("該当する本が存在しません。bookId:${bookId}")
+
+        //貸出中か確認する
+        if(!book.isRental){ throw IllegalArgumentException("未貸出の商品です。bookId:${bookId}")}
+        if(book.rental!!.userId != userId){ throw IllegalArgumentException("未貸出の商品です。bookId:${bookId}")}
+
+        rentalRepository.endRental(bookId)
+    }
 }
